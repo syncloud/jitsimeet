6c62d0a69cd1f0e3585eb5477af3c0c64b12debc
Boris Rybalkin <ribalkin@gmail.com>
2024-07-25 19:30:43 +0100

    LUA_PATH

--- bin/service.prosody.sh
+++ bin/service.prosody.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_DATA/prosody.socket
+export LUA_PATH=$SNAP/prosody/usr/share/lua/5.4
 exec $SNAP/prosody/bin/lua $SNAP/prosody/usr/bin/prosody --config $SNAP_DATA/config/prosody.cfg.lua -F
