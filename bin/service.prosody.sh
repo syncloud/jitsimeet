c76abe773aa4d515a2205a1db439591b446e1dc1
Boris Rybalkin <ribalkin@gmail.com>
2024-07-25 21:05:18 +0100

    fix libs

--- bin/service.prosody.sh
+++ bin/service.prosody.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_DATA/prosody.socket
 export LUA_PATH="$SNAP/prosody/usr/share/lua/5.4/?.lua"
-export LUA_CPATH="$SNAP/prosody/usr/share/lua/5.4/?.so"
+LIBS=$(echo $SNAP/prosody/usr/lib/*-linux-gnu*)
+export LUA_CPATH="$LIBS/lua/5.2/?.so"
 exec $SNAP/prosody/bin/lua $SNAP/prosody/usr/bin/prosody --config $SNAP_DATA/config/prosody.cfg.lua -F
