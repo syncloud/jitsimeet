2bcb0f12d9c1b4eeee4fb0ab85dc3b44e860e8b3
Boris Rybalkin <ribalkin@gmail.com>
2024-07-28 12:54:01 +0100

    fix default plugin dir

--- prosody/build.sh
+++ prosody/build.sh
@@ -7,4 +7,12 @@ mkdir -p ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /bin ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
+cp -r /prosody-plugins ${BUILD_DIR}
+cp -r /prosody-plugins-contrib ${BUILD_DIR}
+cp -r /prosody-plugins-custom ${BUILD_DIR}
 cp --remove-destination ${DIR}/bin/* ${BUILD_DIR}/bin
+
+sed -i "s#CFG_SOURCEDIR=.*;#CFG_SOURCEDIR='/snap/jitsimeet/current/prosody/usr/lib/prosody';#g"  ${BUILD_DIR}/usr/bin/prosody
+sed -i "s#CFG_CONFIGDIR=.*;#CFG_CONFIGDIR='/var/snap/jitsimeet/current/config';#g"  ${BUILD_DIR}/usr/bin/prosody
+sed -i "s#CFG_PLUGINDIR=.*;#CFG_PLUGINDIR='/snap/jitsimeet/current/prosody/usr/lib/prosody/modules/';#g"  ${BUILD_DIR}/usr/bin/prosody
+sed -i "s#CFG_DATADIR=.*;#CFG_DATADIR='/var/snap/jitsimeet/current/data';#g"  ${BUILD_DIR}/usr/bin/prosody
