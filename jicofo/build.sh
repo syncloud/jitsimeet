9fad9120093d968285f22a746d4a0baa89eade25
Boris Rybalkin <ribalkin@gmail.com>
2024-07-29 20:54:16 +0100

    fix java files

--- jicofo/build.sh
+++ jicofo/build.sh
@@ -7,4 +7,12 @@ mkdir -p ${BUILD_DIR}
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
+
+JAVA_DIR=$(echo /usr/lib/jvm/java-17-openjdk-*)
+rm -rf $BUILD_DIR/$JAVA_DIR/conf
+cp -r --dereference $JAVA_DIR/conf $BUILD_DIR/$JAVA_DIR
+rm -rf $BUILD_DIR/$JAVA_DIR/lib
+rm -rf $JAVA_DIR/lib/src.zip
+cp -r --dereference $JAVA_DIR/lib $BUILD_DIR/$JAVA_DIR
+
 cp --remove-destination ${DIR}/bin/* ${BUILD_DIR}/bin
