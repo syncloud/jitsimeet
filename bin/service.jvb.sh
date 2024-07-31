4cec2b39525ad1c7176dcaa5fcab854e810f6c1a
Boris Rybalkin <ribalkin@gmail.com>
2024-07-31 08:49:16 +0100

    fix sip config

--- bin/service.jvb.sh
+++ bin/service.jvb.sh
@@ -7,7 +7,7 @@ exec $SNAP/jvb/bin/java.sh \
   -XX:+UseG1GC \
   -Djdk.tls.ephemeralDHKeySize=2048 \
   -Djdk.net.usePlainDatagramSocketImpl=true \
-  -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/ \
+  -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=$SNAP_DATA \
   -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config \
   -Djava.util.logging.config.file=$SNAP_DATA/config/jvb.log.properties \
   -Dconfig.file=$SNAP_DATA/config/jvb.conf \
