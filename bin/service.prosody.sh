9b85377f9194272fa61a41c21c9d2b63679c0120
Boris Rybalkin <ribalkin@gmail.com>
2024-08-02 07:33:59 +0100

    jvb hostname

--- bin/service.prosody.sh
+++ bin/service.prosody.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 /bin/rm -f $SNAP_DATA/prosody.socket
 LUA="5.4"
 export LUA_PATH="$SNAP/prosody/usr/share/lua/$LUA/?.lua"
@@ -8,3 +8,4 @@ LIBS=$(echo $SNAP/prosody/usr/lib/*-linux-gnu*)
 export LUA_CPATH="$LIBS/lua/$LUA/?.so"
 export LUA_CPATH="$LUA_CPATH;$SNAP/prosody/usr/local/lib/lua/$LUA/?.so"
 exec $SNAP/prosody/bin/lua $SNAP/prosody/usr/bin/prosody --config $SNAP_DATA/config/prosody.cfg.lua -F
+
