a5976e6de4a0b02c80f5c43f75d902fd8f1cef38
Boris Rybalkin <ribalkin@gmail.com>
2024-08-04 12:33:09 +0100

    nginx upstream

--- test/ui.py
+++ test/ui.py
@@ -33,67 +33,11 @@ def test_start(module_setup):
 
 def test_index(selenium):
     selenium.open_app()
-    selenium.find_by(By.XPATH, "//div[contains(.,'Sign in to sync your notes')]")
+    selenium.find_by(By.XPATH, "//button[.='Start meeting']")
     selenium.screenshot('index')
 
 
-def test_register(selenium):
-    selenium.click_by(By.XPATH, "//button[contains(., 'Create free account')]")
-
-    server = selenium.find_by(By.XPATH, "//input[@placeholder='https://api.standardnotes.com']").get_attribute('value')
-    selenium.screenshot('sync-server')
-    assert server == "/api"
-
-    selenium.find_by(By.XPATH, "//input[@type='email']").send_keys('jitsimeet@example.com')
-    selenium.find_by(By.XPATH, "//input[@type='password']").send_keys('pass1234')
-    selenium.screenshot('new-account')
-
-    selenium.click_by(By.XPATH, "//button[text()='Next']")
-    selenium.find_by(By.XPATH, "//input[@type='password']").send_keys('pass1234')
-
-    selenium.click_by(By.XPATH, "//button[contains(., 'Create account')]")
-    selenium.invisible_by(By.XPATH, "//button[text()='Creating account...']")
-    
-    selenium.screenshot('registered')
-
-
-def test_new_note(selenium):
-    selenium.click_by(By.XPATH, "//button[contains(@aria-label, 'Create a new note')]")
-    note = selenium.find_by(By.XPATH, "//textarea")
-    note.send_keys('test note')
-    selenium.find_by(By.XPATH, "//button[contains(@class, 'bg-success')]")
-    selenium.screenshot('note')
-
-
-def test_logout(selenium):
-    selenium.click_by(By.XPATH, "(//footer//button)[1]")
-    selenium.find_by(By.XPATH, "//div[text()='Account']")
-    selenium.screenshot('sign-out-before')
-    selenium.click_by(By.XPATH, "//button[text()='Sign out workspace']")
-    selenium.click_by(By.XPATH, "//button[text()='Sign Out']")
-    selenium.find_by(By.XPATH, "//span[text()='Offline']")
-    selenium.screenshot('sign-out-after')
-
-
-def test_login(selenium):
-    selenium.click_by(By.XPATH, "//button[text()='Sign in']")
-
-    selenium.find_by(By.XPATH, "//input[@type='email']").send_keys('jitsimeet@example.com')
-    selenium.find_by(By.XPATH, "//input[@type='password']").send_keys('pass1234')
-    selenium.click_by(By.XPATH, "//button[text()='Sign in']")
-    selenium.invisible_by(By.XPATH, "//button[text()='Sign in']")
-
-    selenium.invisible_by(By.XPATH, "//button[text()='Signing in...']")
-    selenium.click_by(By.XPATH, "(//footer//button)[1]")
-    selenium.find_by(By.XPATH, "//div[contains(text(), 'signed in as')]")
-    selenium.screenshot('login')
-
-    selenium.click_by(By.XPATH, "//button[text()='Account settings']")
-    selenium.find_by(By.XPATH, "//p[contains(.,'Your Standard Notes subscription will be renewed on')]")
-
-    selenium.screenshot('account')
-
-    selenium.click_by(By.XPATH, "//div[text()='General']")
-    selenium.click_by(By.XPATH, "//h2[.='Offline activation']")
-    selenium.find_by(By.XPATH, "//h4[.='Activate Offline Subscription']")
-    selenium.screenshot('activation')
+def test_meeting(selenium):
+    selenium.find_by(By.ID, "enter_room_field").send_keys('test-meeting')
+    selenium.click_by(By.XPATH, "//button[.='Start meeting']")
+    selenium.find_by(By.XPATH, "//div[contains(.,'Join meeting')]")
